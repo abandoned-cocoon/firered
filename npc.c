13bf275246bc8e2f7ff367d255b7282e7fa40527
knizz <dkreuter@gmail.com>
2013-09-22 10:12:50 +0200

    tile->block, tiletype->role

--- npc.c
+++ npc.c
@@ -35,8 +35,8 @@ struct npc_state {
     u8 oam_id2;
     u8 state;
     u8 sight_distance;
-    u8 tile_to;
-    u8 tile_from;
+    u8 role_block_to;
+    u8 role_block_from;
     u8 field_20;
     u8 field_21;
     u8 field_22;
@@ -91,7 +91,7 @@ void sub_805BCEC(u16 x, u16 y, u8 direction) {
     if (role != 0x66) {
         enum block_reason br = npc_block_way(npc_id, x, y);
         if (block_reason != 0) return 0;
-        if (is_tile_x60(role)) return 0;
+        if (is_role_x60(role)) return 0;
     }
     sub_805CCD0(npc_id, direction);
     return 1;
@@ -150,9 +150,9 @@ bool sub_805CE20_mode_2(struct coro_t* c, struct npc_state* player_npc, struct n
     if (npc_get_bit7_or_const_x10_when_inactive(other_npc) == 0) return 0;
     npc_destruct_when_bit7_is_set(player_npc);
     npc_destruct_when_bit7_is_set(other_npc);
-    npc_hide_and_trainer_flag_clear_on_tile_x66_at_pos(other_npc);
+    npc_hide_and_trainer_flag_clear_on_role_x66_at_pos(other_npc);
     struct coords *o_to = other_npc.to;
-    if_tile_x20_run_trigger_at_position_maybe(o_to->x, o_to->y);
+    if_role_x20_run_trigger_at_position_maybe(o_to->x, o_to->y);
 
     walkrun.lock = 0;
     script_env_2_disable();
@@ -360,11 +360,10 @@ void sub_8063554(struct npc_state *npc, struct oamt *oamt, u8 anim_number) {
 }
 
 // 0806CEA0
-bool onpress_a(struct npc_position *n, u8 tt, u8 d) {
-    // tt = tile type
-    // d = direction in which the player is looking
+bool onpress_a(struct npc_position *n, u8 role, u8 direction) {
+    // direction: direction the player is looking
 
-    u8 *scr = (u8*)onpress_a_get_script(n, tt, d);
+    u8 *scr = (u8*)onpress_a_get_script(n, role, direction);
     if (!scr) return false;
     if (scr != scr_pc1 && scr != scr_pc2) sound_play(5);
     script_start_1(scr);
@@ -372,19 +371,18 @@ bool onpress_a(struct npc_position *n, u8 tt, u8 d) {
 }
 
 // 0806CEE0
-u8 *onpress_a_get_script(struct npc_position *n, u8 tt, u8 d) {
+u8 *onpress_a_get_script(struct npc_position *n, u8 role, u8 direction) {
     u32 scr;
-    if (scr=onpress_a_get_script_npc     (n, tt, d)) return scr;
-    if (scr=onpress_a_get_script_signpost(n, tt, d)) return scr;
-    if (scr=onpress_a_get_script_tile    (n, tt, d)) return scr;
-    if (scr=surf(n, tt, d)) return scr;
+    if (scr=onpress_a_get_script_npc     (n, role, direction)) return scr;
+    if (scr=onpress_a_get_script_signpost(n, role, direction)) return scr;
+    if (scr=onpress_a_get_script_block   (n, role, direction)) return scr;
+    if (scr=surf                         (n, role, direction)) return scr;
     return 0;
 }
 
 // 0806CFF4
-u32 onpress_a_get_script_npc(struct npc_position *n, u8 tt, u8 d) {
-    // tt = tile type
-    // d = direction in which the player is looking
+u32 onpress_a_get_script_npc(struct npc_position *n, u8 role, u8 direction) {
+    // direction: direction the player is looking
 
     u8 npc_id = npc_id_by_pos_and_height(
         n->x,
@@ -392,7 +390,7 @@ u32 onpress_a_get_script_npc(struct npc_position *n, u8 tt, u8 d) {
         n->height
     );
     if (npc_id == MAX_NPCS || npc_states[npc_id].local_id == 0xFF) {
-        if (!is_tile_x80(tt)) return 0;
+        if (!is_role_x80(tt)) return 0;
 
         npc_id = npc_id_by_pos_and_height(
             n->x + directions_i32[d*2+0],
@@ -410,56 +408,54 @@ u32 onpress_a_get_script_npc(struct npc_position *n, u8 tt, u8 d) {
 }
 
 // 0806D1F0
-u32 onpress_a_get_script_tile(struct npc_position *n, u8 tt, u8 d) {
-    // tt = tile type
-    // d = direction in which the player is looking
+u32 onpress_a_get_script_block(struct npc_position *n, u8 role, u8 direction) {
+    // direction: direction the player is looking
 
     var_800C = d;
-    // tile types -> scripts
 
-    if (is_tile_x83(tt)) return 0x081A6955; // (complex stuff. probably for the PC)
-    if (is_tile_x85(tt)) return 0x081A6C32; // (complex stuff)
-    if (is_tile_x81(tt)) return 0x081A7606; // It`s crammed full of POKeMON books.
-    if (is_tile_x82(tt)) return 0x081A760F; // Wow! Tons of POKeMON stuff!
-    if (is_tile_x90(tt)) return 0x081A7618; // It should be packed with all kinds of delicious things to eat!
-    if (is_tile_xA0(tt)) return 0x081A7633; // It`s a machine of some sort. It sure is impressive.
-    if (is_tile_x93(tt)) return 0x081A763C; // It`s a blueprint of some sort. It`s filled with diagrams and text.
-    if (is_tile_xA1(tt)) return 0x081A7621; // It`s the latest video game! It sure looks fun!
-    if (is_tile_xA2(tt)) return 0x081A7645; // There are obvious signs of burglary here|
-    if (is_tile_x97(tt)) return 0x081A762A; // It`s all complicated words and numbers that make no sense|
-    if (is_tile_xA3(tt)) return 0x081C549C; // (complex stuff)
-    if (is_tile_x86(tt)) return 0x081A764E; // There`s a POKeMON on TV! It looks like it`s having fun.
-    if (is_tile_x89(tt)) return 0x081A7657; // Dishes and plates are neatly lined up.
-    if (is_tile_x8A(tt)) return 0x081A7660; // It smells delicious! Somebody`s been cooking here.
-    if (is_tile_x8B(tt)) return 0x081A7669; // It`s a nicely made dresser. It will hold a lot of stuff.
-    if (is_tile_x8C(tt)) return 0x081A7672; // There`s a pile of snacks here.
-    if (is_tile_x94(tt)) return 0x081A767B; // It`s a pretty picture of a POKeMON. It looks like it`s feeling good.
-    if (is_tile_x95(tt)) return 0x081A7684; // What could this machine be? Better not mess around with it!
-    if (is_tile_x96(tt)) return 0x081A768D; // It`s a telephone. Better not use it.
-    if (is_tile_x98(tt)) return 0x081A7696; // It`s an advertising poster about all kinds of products.
-    if (is_tile_x99(tt)) return 0x081A769F; // Oh, that smells tasty! It might get the stomach growling.
-    if (is_tile_x9A(tt)) return 0x081A76A8; // Inside this¦ ¦¦_There`s nothing here!
-    if (is_tile_x9B(tt)) return 0x081A76B1; // It`s a cup with a POKeMON mark on it.
-    if (is_tile_NO1(tt)) return 0x081A76BA; // The window is very well polished.
-    if (is_tile_NO2(tt)) return 0x081A76C3; // Outside the window¦_The sky looks fantastically beautiful.
-    if (is_tile_x9E(tt)) return 0x081A76CC; // Lights in different colors are flashing on and off.
-    if (is_tile_x9F(tt)) return 0x081A76D5; // All sorts of tools are lined up neatly.
-    if (is_tile_x8D_and_d_is_x2(tt, d)) return 0x081BBFD8; // complex stuff.
-    if (is_tile_x8F(tt)) return 0x081A7702; // complex stuff.
-    if (is_tile_x8E_and_d_is_x2(tt, d)) return 0x081BB8A7; // complex stuff.
-    if (is_tile_x91(tt)) {
+    if (is_role_x83(tt)) return 0x081A6955; // (complex stuff. probably for the PC)
+    if (is_role_x85(tt)) return 0x081A6C32; // (complex stuff)
+    if (is_role_x81(tt)) return 0x081A7606; // It`s crammed full of POKeMON books.
+    if (is_role_x82(tt)) return 0x081A760F; // Wow! Tons of POKeMON stuff!
+    if (is_role_x90(tt)) return 0x081A7618; // It should be packed with all kinds of delicious things to eat!
+    if (is_role_xA0(tt)) return 0x081A7633; // It`s a machine of some sort. It sure is impressive.
+    if (is_role_x93(tt)) return 0x081A763C; // It`s a blueprint of some sort. It`s filled with diagrams and text.
+    if (is_role_xA1(tt)) return 0x081A7621; // It`s the latest video game! It sure looks fun!
+    if (is_role_xA2(tt)) return 0x081A7645; // There are obvious signs of burglary here|
+    if (is_role_x97(tt)) return 0x081A762A; // It`s all complicated words and numbers that make no sense|
+    if (is_role_xA3(tt)) return 0x081C549C; // (complex stuff)
+    if (is_role_x86(tt)) return 0x081A764E; // There`s a POKeMON on TV! It looks like it`s having fun.
+    if (is_role_x89(tt)) return 0x081A7657; // Dishes and plates are neatly lined up.
+    if (is_role_x8A(tt)) return 0x081A7660; // It smells delicious! Somebody`s been cooking here.
+    if (is_role_x8B(tt)) return 0x081A7669; // It`s a nicely made dresser. It will hold a lot of stuff.
+    if (is_role_x8C(tt)) return 0x081A7672; // There`s a pile of snacks here.
+    if (is_role_x94(tt)) return 0x081A767B; // It`s a pretty picture of a POKeMON. It looks like it`s feeling good.
+    if (is_role_x95(tt)) return 0x081A7684; // What could this machine be? Better not mess around with it!
+    if (is_role_x96(tt)) return 0x081A768D; // It`s a telephone. Better not use it.
+    if (is_role_x98(tt)) return 0x081A7696; // It`s an advertising poster about all kinds of products.
+    if (is_role_x99(tt)) return 0x081A769F; // Oh, that smells tasty! It might get the stomach growling.
+    if (is_role_x9A(tt)) return 0x081A76A8; // Inside this¦ ¦¦_There`s nothing here!
+    if (is_role_x9B(tt)) return 0x081A76B1; // It`s a cup with a POKeMON mark on it.
+    if (is_role_NO1(tt)) return 0x081A76BA; // The window is very well polished.
+    if (is_role_NO2(tt)) return 0x081A76C3; // Outside the window¦_The sky looks fantastically beautiful.
+    if (is_role_x9E(tt)) return 0x081A76CC; // Lights in different colors are flashing on and off.
+    if (is_role_x9F(tt)) return 0x081A76D5; // All sorts of tools are lined up neatly.
+    if (is_role_x8D_and_d_is_x2(tt, d)) return 0x081BBFD8; // complex stuff.
+    if (is_role_x8F(tt)) return 0x081A7702; // complex stuff.
+    if (is_role_x8E_and_d_is_x2(tt, d)) return 0x081BB8A7; // complex stuff.
+    if (is_role_x91(tt)) {
         (*(u8*)03000FA1) = 1;
         return 0x081A76F0; // INDIGO PLATEAU_The ultimate goal of TRAINERS! POKeMON LEAGUE HQ
     }
-    if (is_tile_x92(tt)) {
+    if (is_role_x92(tt)) {
         (*(u8*)03000FA1) = 1;
         return 0x081A76F9; // INDIGO PLATEAU_The highest POKeMON authority! POKeMON LEAGUE HQ
     }
-    if (is_tile_x88_and_d_is_x2(tt, d)) {
+    if (is_role_x88_and_d_is_x2(tt, d)) {
         (*(u8*)03000FA1) = 1;
         return 0x081A76DE; // All your item needs fulfilled! POKeMON MART
     }
-    if (is_tile_x87_and_d_is_x2(tt, d)) {
+    if (is_role_x87_and_d_is_x2(tt, d)) {
         (*(u8*)03000FA1) = 1;
         return 0x081A76E7; // Heal Your POKeMON! POKeMON CENTER
     }
@@ -467,7 +463,7 @@ u32 onpress_a_get_script_tile(struct npc_position *n, u8 tt, u8 d) {
 }
 
 // 0806DE28
-void npc_hide_and_trainer_flag_clear_on_tile_x66_at_pos(stuct npc_state *npc) {
+void npc_hide_and_trainer_flag_clear_on_role_x66_at_pos(stuct npc_state *npc) {
     u8 role = cur_mapdata_block_role_at(npc->to.x, npc->to.y);
     if (role != 0x66) return;
     sound_play(0x25);
