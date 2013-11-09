f94ea9ba4fb64b12d49bbf35fcef1713747879af
Example <example@example.com>
2013-11-09 22:57:40 +0100

    #25

--- bag.c
+++ bag.c
@@ -1,3 +1,63 @@
+// 0809A8A4
+itemid itemid_sanitize(itemid i) {
+	if (i >= NUM_ITEMS) return 0;
+	return i;
+}
+
+// 0809A8BC
+struct item *item_by_id(itemid i) {
+	i = itemid_sanitize(i);
+	return &items[i];
+}
+
+// 0809A8DC
+void itemid_get_number(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].number;
+}
+
+// 0809A900
+u16 itemid_get_market_price(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].marked_price;
+}
+
+// 0809A924
+u8 itemid_get_x12(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].field_12;
+}
+
+// 0809A948
+u8 itemid_get_quality(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].quality;
+}
+
+// 0809A96C
+const char *itemid_get_description(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].description;
+}
+
+// 0809A990
+bool itemid_can_not_be_held(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].can_not_be_held;
+}
+
+// 0809A9B4
+u8 itemid_get_x19(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].field_19;
+}
+
+// 0809A9D8
+void itemid_get_pocket_number(itemid i) {
+	i = itemid_sanitize(i);
+	return items[i].get_pocket_number;
+}
+
 // 08108CFC
 void coro_WIN0V_open_close_above(coro_id c) {
 	typedef struct {
