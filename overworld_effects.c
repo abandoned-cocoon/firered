619dc37daf5c604d13a1d1246b37aa6664fb705a
David Kreuter <dkreuter@gmail.com>
2014-01-18 13:23:58 +0100

    overworld effects

---
+++ overworld_effects.c
@@ -0,0 +1,37 @@
+enum OEC {
+	OEC_LOAD_GFX = 0,
+	OEC_LOAD_PAL1 = 1,
+	OEC_LOAD_PAL2,
+	OEC_CALL,
+	OEC_END,
+	OEC_GFX_PAL1_AND_CALL,
+	OEC_GFX_AND_CALL,
+	OEC_PAL1_AND_CALL
+};
+
+// 081D96AC
+u8 oe_081D97C4[] = {
+	OEC_CALL, &sub_08082544,
+	OEC_END
+};
+
+// 081D97CA
+u8 oe_cut[] = {
+	OEC_CALL, &sub_080979A0
+	OEC_END
+};
+
+// 081D97D0
+u8 oe_cut[] = {
+	OEC_CALL, &sub_080979F0
+	OEC_END
+};
+
+// 081D97D6
+u8 oe_081D97D6[] = {
+	OEC_CALL, 
+	OEC_END
+};
+
+// 083CBE30
+void 
