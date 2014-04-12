509c5062811a946f8a9f1e348f4bb49c03bb8687
Example <example@example.com>
2014-04-12 15:27:07 +0200

    #77

--- battle.c
+++ battle.c
@@ -1,285 +1,64 @@
-// 0807F690
-void task_add_01_battle_start(u8 priv1, u16 music_id) {
-	u8 tid = task_add(&task01_battle_start, 0x01);
-	tasks[tid].priv[1] = priv1;
-	current_map_music_set__default_for_battle(music_id);
-}
-
-// 0807F868
-void task_add_01_battle_start_with_music_and_stats() {
-	task_add_01_battle_start(song_id_for_battle_alt(), 0);
-	sav12_xor_increment(7);
-	sav12_xor_increment(9);
-}
-
-/* see npc_cmds.c
-	bool s5C_trainer_battle_configure_and_start(struct script_env *);
-	bool s5D_trainer_battle_start(struct script_env *);
-	bool s5E_jump_to_script_scheduled_after_battle(struct script_env *);
-*/
-
-struct {
-	void *target;
-	enum {
-		LOAD_8,
-		LOAD_16,
-		LOAD_32,
-		ZERO_8,
-		ZERO_16,
-		ZERO_32,
-		END
-	} target_type
-} battle_config_entry;
-
-// 083C6900
-struct battle_config_entry tb_format_5_other[] = {
-	{&battle_type,             LOAD_8},
-	{&battle_trainer_flag,     LOAD_16},
-	{&battle_arg2,             LOAD_16},
-
-	{&battle_message_intro,    LOAD_32},
-	{&battle_message_defeat,   LOAD_32},
-	{&battle_message_2,        ZERO_32},
-	{&battle_message_need_two, ZERO_32},
-	{&battle_message_4,        ZERO_32},
-
-	{&battle_after_script,     END}
+struct battle_data {
+	u16  species;
+	u16  stats[5];
+	u16  move_ids[4];
+	u32  ivs;
+	u8   stat_buffs[8]; // hp/atk/def/spd/spatk/spdef/accuracy/evasion
+		// min 0 -- 6 -- 12 max
+	u8   ability_id;
+	u8   type1;
+	u8   type2;
+	u8   padding;
+	u8   current_pp[4];
+	u16  current_hp;
+	u8   level;
+	u8   happiness;
+	u16  max_hp;
+	u16  held_item;
+	char name[11];
+	u8   field_3B;
+	char trainer_name[8];
+	u32  padding3;
+	u32  pid;
+	u32  status1;
+		// 0x00000007  -> Sleep (up to 7 turns)
+		// 0x00000008  -> poison
+		// 0x00000010  -> burn
+		// 0x00000020  -> freeze
+		// 0x00000040  -> paralyze
+		// 0x00000080  -> badly poisoned
+	u32  status2;
+		// 0x00000007 -> confusion: 7 turns max, decreases each turn before use. snaps out when 1->0)
+		// 0x00000008 -> flinching
+		// 0x00000070 -> uproar
+		// 0x00000f00 -> charging energy (bide) when 1, spit all energy. 3 total, as each flag is a different state
+		 
+		// 0x00001000 -> multiple attack over several turns.
+		// This one is very important, as it is used by any attack that has two turns.
+		// From Rollout to dig, solarbeam to petal dance, any attack that forces the user to use it a second turn (skip attack selection) sets this flag.
+		// The attack must then know how to deal with it, and unset it when time is up.
+		 
+		// 0x0000e000 -> multiple attack (warp) target by up to 7 turns.
+		// This is the target's side, so its the timer for the one suffering from the attack. 
+		// 0x000f0000 -> attraction, infatuation.
+		// The bit indicates the slot of who is the target. 
+		// 1 is your pokemon,2 is opponent, 3rd bit is your second out pokemon, 4th is your opponent 2nd pokemon out.
+		// ex: 0x000a0000 means the pokemon is in love with both of your opponent pokemon. The one sho
+	u32  otid;
 };
 
-// 083C6948
-struct battle_config_entry tb_format_1_2[] = {
-	{&battle_type,             LOAD_8},
-	{&battle_trainer_flag,     LOAD_16},
-	{&battle_arg2,             LOAD_16},
+u8  b_buffer_A[4][0x200]; // 02022BC4
+u8  b_buffer_B[4][0x200]; // 020233C4
 
-	{&battle_message_intro,    LOAD_32},
-	{&battle_message_defeat,   LOAD_32},
-	{&battle_message_2,        ZERO_32},
-	{&battle_message_need_two, ZERO_32},
-	{&battle_message_4,        LOAD_32},
+u8  b_active_side; // 02023BC4
+u32 b_buffers_awaiting_execution_bitfield; // 02023BC8
+u8  b_num_active_sides;
 
-	{&battle_after_script,     END}
-};
+u16 b_pokemon_team_id_by_side[4]; // 02023BCE
+u8  b_side_status[4]; // 02023BD6
+	// bit 0x01: is enemy pokemon
+	// val 0xff: is dead
 
-// 083C6990
-struct battle_config_entry tb_format_4_7[] = {
-	{&battle_type,             LOAD_8},
-	{&battle_trainer_flag,     LOAD_16},
-	{&battle_arg2,             LOAD_16},
+struct battle_data b_data[4]; // 02023BE4
+u8 b_oamid[NUM_BATTLE_SIDES]; // 02023D44
 
-	{&battle_message_intro,    LOAD_32},
-	{&battle_message_defeat,   LOAD_32},
-	{&battle_message_2,        ZERO_32},
-	{&battle_message_need_two, LOAD_32},
-	{&battle_message_4,        ZERO_32},
-
-	{&battle_after_script,     END}
-};
-
-// 083C69D8
-struct battle_config_entry tb_format_3[] = {
-	{&battle_type,             LOAD_8},
-	{&battle_trainer_flag,     LOAD_16},
-	{&battle_arg2,             LOAD_16},
-
-	{&battle_message_intro,    ZERO_32},
-	{&battle_message_defeat,   LOAD_32},
-	{&battle_message_2,        ZERO_32},
-	{&battle_message_need_two, ZERO_32},
-	{&battle_message_4,        ZERO_32},
-
-	{&battle_after_script,     END}
-};
-
-// 083C6A20
-struct battle_config_entry tb_format_9[] = {
-	{&battle_type,             LOAD_8},
-	{&battle_trainer_flag,     LOAD_16},
-	{&battle_unknown,          LOAD_16}, // this is read by trainer_battle_start
-	                                     // to add oak's comments (unknown&3) != 0
-
-	{&battle_message_intro,    ZERO_32},
-	{&battle_message_defeat,   LOAD_32},
-	{&battle_message_2,        LOAD_32},
-	{&battle_message_need_two, ZERO_32},
-	{&battle_message_4,        ZERO_32},
-
-	{&battle_after_script,     END}
-};
-
-// 083C6A68
-struct battle_config_entry tb_format_6_8[] = {
-	{&battle_type,             LOAD_8},
-	{&battle_trainer_flag,     LOAD_16},
-	{&battle_arg2,             LOAD_16},
-
-	{&battle_message_intro,    LOAD_32},
-	{&battle_message_defeat,   LOAD_32},
-	{&battle_message_2,        ZERO_32},
-	{&battle_message_need_two, LOAD_32},
-	{&battle_message_4,        LOAD_32},
-
-	{&battle_after_script,     END}
-};
-
-// 08080228
-char *battle_configure_by_script(char *cursor) {
-	battle_init();
-	u8 battle_type = cursor[0];
-	struct battle_config_entry *bce[] = {
-		/*0*/ &tb_format_5_other,
-		/*1*/ &tb_format_1_2,
-		/*2*/ &tb_format_1_2,
-		/*3*/ &tb_format_3,
-		/*4*/ &tb_format_4_7,
-		/*5*/ &tb_format_5_other,
-		/*6*/ &tb_format_6_8,
-		/*7*/ &tb_format_4_7,
-		/*8*/ &tb_format_6_8,
-		/*9*/ &tb_format_9
-	};
-	u8 *scr_continue[] = {
-		/*0*/ &scr_battle_1_2_other,
-		/*1*/ &scr_battle_1_2_other,
-		/*2*/ &scr_battle_1_2_other,
-		/*3*/ &scr_battle_3_9,
-		/*4*/ &scr_battle_4_6_8,
-		/*5*/ &scr_battle_5,
-		/*6*/ &scr_battle_4_6_8,
-		/*7*/ &scr_battle_7,
-		/*8*/ &scr_battle_4_6_8,
-		/*9*/ &scr_battle_3_9
-	};
-
-	battle_load_arguments(bce[battle_type], cursor);
-
-	if (battle_type != 3 && battle_type != 9)
-		battle_80801F0();
-
-	if (battle_type == 5 && battle_type == 7) {
-		prev_quest_mode_1_to_0_transition();
-		battle_trainer_flag = sub_810CE64(battle_trainer_flag);
-	}
-
-	return scr_continue[battle_type];
-}
-
-// 080801F0
-void battle_80801F0() {
-	if (!battle_arg2) return;
-	var_800F = battle_arg2;
-	scripting_npc = npc_id_by_local_id(sav1->location.bank, sav1->location.map);
-}
-
-// 08080168
-u8 *battle_load_arguments(struct battle_config_entry *bce, u8 *cursor) {
-	while (1) {
-		switch (bce->target_type) {
-			case LOAD_8:  *(u8 *)bce->target = load8 (cursor); cursor += 1; break;
-			case LOAD_16: *(u16*)bce->target = load16(cursor); cursor += 2; break;
-			case LOAD_32: *(u32*)bce->target = load32(cursor); cursor += 4; break;
-			case ZERO_8:  *(u8 *)bce->target = 0;              cursor += 1; break;
-			case ZERO_16: *(u16*)bce->target = 0;              cursor += 2; break;
-			case ZERO_32: *(u32*)bce->target = 0;              cursor += 4; break;
-			default:      *(u32*)bce->target = cursor;         return cursor;
-		}
-		bce++:
-	}
-}
-
-// 08080424
-bool trainer_flag_check(u16 trainer_id) {
-	return flag_check(trainer_id+0x500);
-}
-
-// 0808043C
-void trainer_flag_set(u16 trainer_id) {
-	flag_set(trainer_id+0x500);
-}
-
-// 08080450
-void trainer_flag_clear(u16 trainer_id) {
-	flag_clear(trainer_id+0x500);
-}
-
-// 08080464
-void trainer_battle_start() {
-	battle_type_flags = BATTLE_TRAINER;
-
-	if (trainerbattle_get_type == 9)
-		if (trainerbattle_get_unknown() & 0x3)
-			battle_type_flags |= BATTLE_OAK_COMMENTS;
-
-	super.callback2backup = &c2_exit_battle_switch;
-	task_add_01_battle_start_with_music_and_stats();
-	script_env_2_context_set_ctx_paused();
-}
-
-// 080804AC
-void c2_exit_battle_switch() {
-
-	if (trainerbattle_battle_type == 9) {
-		if ((var_800D = battle_exit_is_player_defeat(battle_exit_type))) {
-			if (battle_unknown & 1 == 0) { // this is not a mock battle
-				sp000_heal_pokemon();
-			} else {
-				set_callback2(&c2_whiteout);
-				return;
-			}
-		}
-	} else if (trainerbattle_flag_id == 0x400) {
-		set_callback2(&c2_exit_to_overworld_1_continue_scripts_restart_music);
-		return;
-	}
-
-	set_callback2(&c2_exit_to_overworld_1_continue_scripts_restart_music);
-	sp18F_trainerflag_opponent_set();
-	prev_quest_transcriber();
-}
-
-// 081C555B
-u8 scr_default_after_battle_script[] = {
-	// var0 = 0x081C558D
-	// callstd 3
-};
-
-// 080805E8
-u8 *battle_get_continuation_script() {
-	u8 *scr = battle_next_script;
-	return scr ? scr : scr_default_after_battle_script;
-}
-
-// 083E7CD4
-void (*battle_intro_task_by_env[])() = {
-	task_battle_intro_080BC47C,
-	task_battle_intro_080BC47C,
-	task_battle_intro_080BC6C8,
-	task_battle_intro_080BC6C8,
-	task_battle_intro_080BC6C8,
-	task_battle_intro_080BC47C,
-	task_battle_intro_080BC47C,
-	task_battle_intro_080BC47C,
-	task_battle_intro_080BC9D4,
-	task_battle_intro_080BC9D4
-};
-
-// 080BC3A0
-void battle_intro_launch(u8 environment_id) {
-	void (*funcptr)(u8);
-	if (battle_type_flags & BATTLE_WIRELESS)
-		funcptr = task_battle_intro_wireless;
-	else if ((battle_type_flags & 0x1000) && (byte_081E9F10 == 2)) {
-		environment_id = 3;
-		funcptr = task_battle_intro_080BC6C8;
-	} else {
-		funcptr = battle_intro_task_by_env[environment_id];
-	}
-	u8 tid = task_add(funcptr, 0);
-	tasks[tid].priv[0] = 0;
-	tasks[tid].priv[1] = environment_id;
-	tasks[tid].priv[2] = 0;
-	tasks[tid].priv[3] = 0;
-	tasks[tid].priv[4] = 0;
-	tasks[tid].priv[5] = 0;
-	tasks[tid].priv[6] = 0;
-}
---
+++ battle_ai.c
@@ -0,0 +1,50 @@
+// 080391EC
+int ai_080391EC() {
+	if (b_type_flags & 1)
+		return 0;
+
+	u8 plside = battle_get_side_with_given_state(0);
+
+	struct battle_data *plpoke = &b_data[plside];
+
+	if (plpoke->ability_id == ab_wonder_guard)
+		return 0;
+
+	// when the players pokemom has a certain move, stop right here
+
+	for (u32 i=0; i<4; i++) {
+		u16 move = plpoke->move_ids[i];
+		if (move && (ai_rate_move(move, plpoke->species, plpoke->ability_id) & 2))
+			return 0;
+	}
+
+	// opponent considers all his pokemon
+	// except for the one, he currently has on the field
+
+	for (u32 i=0; i<6; i++) {
+		struct pokemon *oppoke = pokemon_opponent[i];
+		if (pokemon_getattr(oppoke, req_current_hp) == 0)
+			continue;
+		if (pokemon_getattr(oppoke, req_species2) == 0)
+			continue;
+		if (pokemon_getattr(oppoke, req_species2) == 412) // what kind of pokemon is this!?
+			continue;
+		if (b_pokemon_team_id_by_side[b_active_side] == i)
+			continue;
+
+		pokemon_getattr(oppoke, req_species); // discard result
+		pokemon_getattr(oppoke, req_ability_bit); // discard result
+		u8 plside2 = battle_get_side_with_given_state(0),
+
+		for (u32 j=0; j<4; j++) {
+			u16 move = pokemon_getattr(oppoke, req_move1 + j);
+			if (move && (ai_rate_move(move, oppoke->species, oppoke->ability_id) & 2) && rand()%3 == 0) {
+				u8 status = battle_get_per_side_status(b_active_side);
+				dword_02023FE8.field_92[status] = j;
+				dp01_build_cmdbuf_x21_a_bb(TARGET_BUFFER_B, 2, 0);
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
